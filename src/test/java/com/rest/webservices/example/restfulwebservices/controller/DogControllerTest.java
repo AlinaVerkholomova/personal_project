797f8e57f7e2c4a5dae6cffcbde6050e78f88d5e
Alina Verkholomova <alina.verkholomova@accenture.com>
2023-04-25 14:34:41 +0300

    develop controller and service tests

---
+++ src/test/java/com/rest/webservices/example/restfulwebservices/controller/DogControllerTest.java
@@ -0,0 +1,186 @@
+package com.rest.webservices.example.restfulwebservices.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest.webservices.example.restfulwebservices.controllers.DogController;
+import com.rest.webservices.example.restfulwebservices.model.DogDto;
+import com.rest.webservices.example.restfulwebservices.service.DogService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(DogController.class)
+public class DogControllerTest {
+
+    public static String URL = "/api/v1/dogs";
+    private static final Long VALID_ID = 1L;
+
+    @MockBean
+    private DogService service;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private DogDto dogDto;
+
+    private List<DogDto> dogDtoList;
+
+    private Optional<DogDto> dogDtoOptional;
+
+    @BeforeEach
+    public void setUp(){
+        dogDto = new DogDto();
+        dogDto.setId(VALID_ID);
+        dogDto.setNickname("Archie");
+        dogDto.setBreed("none");
+        dogDto.setAge(2);
+
+        dogDtoList = new ArrayList<>(Arrays.asList(dogDto));
+        dogDtoOptional = Optional.of(dogDto);
+    }
+
+    @Test
+    public void getAllDogsHttpRequest() throws Exception {
+
+        when(service.findAll()).thenReturn(dogDtoList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dogDto)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(VALID_ID))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].nickname").value("Archie"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].breed").value("none"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(2))
+                .andReturn();
+        verify(service, times(1)).findAll();
+    }
+
+    @Test
+    public void getAllDogsHttpEmptyResponse() throws Exception {
+        List<DogDto> dogDto = new ArrayList<>();
+        when(service.findAll()).thenReturn(dogDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(URL)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void saveDogHttpRequest() throws Exception {
+        when(service.saveDog(dogDto)).thenReturn(dogDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dogDto)))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(VALID_ID))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("Archie"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.breed").value("none"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(2))
+                .andReturn();
+
+        verify(service, times(1)).saveDog(dogDto);
+    }
+
+    @Test
+    public void saveDogsHttpRequestEmpty() throws Exception {
+        dogDto = new DogDto();
+
+        when(service.saveDog(dogDto)).thenReturn(dogDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void testFindDogByIdValid() throws Exception {
+        when(service.findDogById(VALID_ID)).thenReturn(dogDtoOptional);
+
+        ResultActions mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get(URL + "/1"))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(VALID_ID))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nickname").value("Archie"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.breed").value("none"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(2))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).findDogById(VALID_ID);
+    }
+
+    @Test
+    void testFindDogByIdInvalid() throws Exception {
+        dogDtoOptional.get().setId(null);
+
+        when(service.findDogById(null)).thenReturn(Optional.empty());
+
+        ResultActions mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .get(URL + null)
+                        .content(asJsonString(dogDtoOptional))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(service, times(0)).findDogById(null);
+    }
+
+    @Test
+    void testDeleteDogValidId() throws Exception {
+        when(service.findDogById(anyLong())).thenReturn(dogDtoOptional);
+        this.mockMvc.perform(MockMvcRequestBuilders
+                        .delete(URL + "/1").content(asJsonString(dogDtoOptional))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+        verify(service, times(1)).deleteDogById(anyLong());
+
+    }
+
+    @Test
+    void testDeleteDogInvalidId() throws Exception {
+        dogDtoOptional.get().setId(null);
+
+        when(service.findDogById(null)).thenReturn(dogDtoOptional);
+
+        ResultActions mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                        .delete(URL + null)
+                        .content(asJsonString(dogDtoOptional))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+
+        verify(service, times(0)).deleteDogById(null);
+    }
+
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+
+        }
+    }
+
+}
---
+++ src/test/java/com/rest/webservices/example/restfulwebservices/service/impl/DogServiceImplTest.java
@@ -0,0 +1,130 @@
+package com.rest.webservices.example.restfulwebservices.service.impl;
+
+import com.rest.webservices.example.restfulwebservices.exception.DogException;
+import com.rest.webservices.example.restfulwebservices.mappers.DogMapStructMapper;
+import com.rest.webservices.example.restfulwebservices.model.DogDto;
+import com.rest.webservices.example.restfulwebservices.model.DogEntity;
+import com.rest.webservices.example.restfulwebservices.repository.DogRepository;
+import com.rest.webservices.example.restfulwebservices.service.DogServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static com.rest.webservices.example.restfulwebservices.service.LogMessageStore.DOG_WITH_ID_NOT_FOUND;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class DogServiceImplTest {
+
+    private static final Long VALID_ID = 1L;
+    private static final Long INVALID_ID = -1L;
+    @InjectMocks
+    private DogServiceImpl service;
+    @Mock
+    private DogRepository repository;
+
+    @Mock
+    private DogMapStructMapper mapper;
+
+    private DogEntity dogEntity;
+
+    private DogDto dogDto;
+
+    private List<DogEntity> dogEntityList;
+
+    private List<DogDto> dogDtoList;
+
+    @BeforeEach
+    public void setUp() {
+        dogEntity = new DogEntity(
+                VALID_ID,
+                "sdfsf",
+                "sfdfds",
+                5
+        );
+        dogDto = new DogDto(
+                VALID_ID,
+                "sdfsf",
+                "sfdfds",
+                5
+        );
+
+        dogDtoList = new ArrayList<>(Arrays.asList(dogDto));
+        dogEntityList = new ArrayList<>(Arrays.asList(dogEntity));
+
+    }
+
+    @Test
+    public void saveDogTest() throws Exception {
+        when(repository.save(dogEntity)).thenReturn(dogEntity);
+        when(mapper.entityToDto(dogEntity)).thenReturn(dogDto);
+        when(mapper.dtoToEntity(dogDto)).thenReturn(dogEntity);
+        DogDto dogSaved = service.saveDog(dogDto);
+        assertEquals(dogDto,dogSaved);
+        verify(repository, times(1)).save(dogEntity);
+
+    }
+    @Test
+    public void getAllDogsWhenHttpRequestSuccessful(){
+        when(repository.findAll()).thenReturn(dogEntityList);
+        when(mapper.entityToDto(dogEntity)).thenReturn(dogDto);
+
+        List<DogDto> dogDtoList = service.findAll();
+
+        assertEquals(1, dogDtoList.size());
+        assertEquals(1, dogDtoList.get(0).getId());
+        assertEquals("sdfsf", dogDtoList.get(0).getNickname());
+    }
+
+    @Test
+    public void getAllWorkplacesWhenEmptyArrayList() {
+        when(repository.findAll()).thenReturn(new ArrayList<>());
+
+        List<DogDto> dogList = service.findAll();
+
+        assertEquals(0, dogList.size());
+        assertTrue(dogList.isEmpty());
+    }
+
+    @Test
+    void testFindDogByValidId() throws Exception {
+        when(repository.findById(VALID_ID)).thenReturn(Optional.of(dogEntity));
+        when(mapper.entityToDto(dogEntity)).thenReturn(dogDto);
+        Optional<DogDto> returnedDog = service.findDogById(dogDto.getId());
+        assertEquals(dogDto.getId(), returnedDog.get().getId());
+        assertEquals(dogDto.getNickname(), returnedDog.get().getNickname());
+        assertEquals(dogDto.getBreed(), returnedDog.get().getBreed());
+        assertEquals(dogDto.getAge(), returnedDog.get().getAge());
+        verify(repository, times(1)).findById(VALID_ID);
+    }
+    @Test
+    void testFindDogByInvalidId() throws Exception {
+        when(repository.findById(INVALID_ID)).thenReturn(Optional.empty());
+        Assertions.assertFalse(service.findDogById(INVALID_ID).isPresent());
+        verify(repository, times(1)).findById(INVALID_ID);
+    }
+
+
+    @Test
+    public void testDeleteDogByInvalidId3() throws DogException {
+        // given
+        Long invalidId = INVALID_ID;
+        given(repository.findById(invalidId)).willReturn(Optional.empty());
+
+        // when, then
+        assertThrows(DogException.class, () -> service.deleteDogById(invalidId), "Dog not found with id: " + invalidId);
+    }
+
+}
